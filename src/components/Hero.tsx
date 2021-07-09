bd8780837b236a37edca6d2f89f9726bef11ae46
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-10 01:31:03 +0200

    feat: Created Home Hero Section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,20 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Avatar, Button, VStack, Heading, Text, Center, useColorModeValue } from "@chakra-ui/react";
+import { FiArrowRightCircle } from "react-icons/fi";
 
-export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, #7928CA, #FF0080)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-)
-
-Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
-}
+export const Hero = () => (
+	<Center>
+		<VStack spacing="6" textAlign="center">
+			<Avatar name="✌" size="2xl" src="assets/images/memoji-thumbs-up.png" bg="transparent" />
+			<Text fontSize="xl" fontWeight="medium" color={useColorModeValue("blackAlpha.900", "whiteAlpha.900")} lineHeight="shorter">
+				👋 Hi, I'm Bassem.
+			</Text>
+			<Heading as="h2" size="xl" maxWidth="xl" lineHeight="shorter">
+				I build <span className="underline">sophisticated</span> websites and web applications.
+			</Heading>
+			<Text fontSize="md" maxWidth="lg" color={useColorModeValue("blackAlpha.700", "whiteAlpha.700")} lineHeight="base">
+				I’m a freelance designer and developer. I help companies ship quality software for happy customers. Let's celebrate your success together!
+			</Text>
+			<Button rightIcon={<FiArrowRightCircle />}>See My Projects</Button>
+		</VStack>
+	</Center>
+);
