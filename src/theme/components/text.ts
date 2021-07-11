00ac6598069142feeded6ecc57e3228aa0137233
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-11 16:26:46 +0200

    feat: Added Text error variant

--- src/theme/components/text.ts
+++ src/theme/components/text.ts
@@ -2,11 +2,14 @@ import { mode } from "@chakra-ui/theme-tools";
 
 export const Text = {
 	variants: {
-        light: props => ({
-            color: mode("blackAlpha.900", "whiteAlpha.900")(props),
+		light: props => ({
+			color: mode("blackAlpha.900", "whiteAlpha.900")(props),
 		}),
-        lighter: props => ({
-            color: mode("gray.600", "gray.300")(props),
-        }),
+		lighter: props => ({
+			color: mode("gray.600", "gray.300")(props),
+		}),
+		error: props => ({
+			color: mode("red.500", "red.200")(props),
+		}),
 	},
 };
