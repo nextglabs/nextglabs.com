a0bcf8d5114f6728bdfe3ab62a0179442f9514d8
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-11 15:31:31 +0200

    feat: Added GraphQL & SWR

---
+++ src/config/swr.ts
@@ -0,0 +1,6 @@
+import { request } from "graphql-request";
+export const fetcher = (query: string) => request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, query);
+
+export const swrConfigOptions = {
+	fetcher,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,24 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
+import { SWRConfig } from "swr";
 
 import theme from "@/theme";
 import { GlobalFonts } from "@/theme/fonts";
 import { Layout } from "@/layout";
 import { configSEO } from "@/config/seo";
+import { swrConfigOptions } from "@/config/swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<DefaultSeo {...configSEO} />
 			<GlobalFonts />
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<SWRConfig value={swrConfigOptions}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</SWRConfig>
 		</ChakraProvider>
 	);
 }
