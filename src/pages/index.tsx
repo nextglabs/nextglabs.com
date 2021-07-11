b1ca3aba280a319d6fcfd983269d622631771480
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-11 13:06:42 +0200

    Updated Home to include Projects & CTA sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import { Divider } from "@chakra-ui/react";
 import { Hero } from "@/components/Hero";
+import { Projects } from "@/components/Projects";
+import { CallToAction } from "@/components/CallToAction";
 
 const Index = () => (
 	<>
 		<Hero />
+		<Divider mt="12" />
+		<Projects />
+		<CallToAction />
 	</>
 );
 
