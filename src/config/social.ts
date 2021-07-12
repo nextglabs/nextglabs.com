aaaa300ec3d4418872fb03848480adcbda13684d
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-12 12:31:32 +0200

    Updated portfolio url

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,7 +1,7 @@
 export const SOCIAL = {
 	// Github
 	GITHUB_ACCOUNT_URL: "https://github.com/nextglabs",
-	GITHUB_PORTFOLIO_URL: "https://github.com/nextglabs/nextglabs-next",
+	GITHUB_PORTFOLIO_URL: "https://github.com/nextglabs/nextglabs-portfolio",
 
 	// Medium
 	MEDIUM_ACCOUNT_URL: "https://nextglabs.medium.com",
