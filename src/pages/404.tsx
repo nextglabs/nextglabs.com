7d77edd24b4a628de9797b11ec1a94d622ae8ae3
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-12 12:18:54 +0200

    fix: Fixed broken go back in some cases & improved UX

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button, Heading, VStack, Text } from "@chakra-ui/react";
 import { FiArrowLeft } from "react-icons/fi";
 import { useRouter } from "next/router";
@@ -5,15 +6,39 @@ import { Memoji } from "@/components/Memoji";
 
 export default function FourOhFour() {
 	const router = useRouter();
+	const [buttonText, setButtonText] = useState("Go Back");
+
+	useEffect(() => {
+		if (window) {
+			// Back to previous page is unlikely to work in this case.
+			// Let's update the button text for better UX
+			if (window.history.length <= 2) {
+				setButtonText("Go to Homepage");
+			}
+		}
+	}, []);
+
+	const handleGoBack = () => {
+		if (window) {
+			if (window.history.length > 2) {
+				return router.back();
+			}
+			// In this case the user came directly to an undefined page,
+			// (ie: typing the wrong url in a new browser window)
+			// So we redirect to the home page.
+			return router.push("/");
+		}
+	};
+
 	return (
 		<VStack spacing="6" mt="24" textAlign="center">
 			<Heading as="h1" size="lg">
-				Ooh &amp;$!#%. This page was not found.
+				Oh &amp;$!#%. This page was not found.
 			</Heading>
 			<Memoji src="assets/images/memoji-surprised.png" />
-			<Text>But hey, the button below should work, I mean theoretically...</Text>
-			<Button leftIcon={<FiArrowLeft />} onClick={() => router.back()}>
-				Go back
+			<Text variant="lighter">But hey, the button below should work, I mean theoretically...</Text>
+			<Button leftIcon={<FiArrowLeft />} onClick={handleGoBack}>
+				{buttonText}
 			</Button>
 		</VStack>
 	);
