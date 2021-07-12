f0064a7687057230dd0092cc835164433c6881f2
Bassem <42464680+nextglabs@users.noreply.github.com>
2021-07-12 13:18:32 +0200

    Updated URLs

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,7 +1,7 @@
 export const SOCIAL = {
 	// Github
 	GITHUB_ACCOUNT_URL: "https://github.com/nextglabs",
-	GITHUB_PORTFOLIO_URL: "https://github.com/nextglabs/nextglabs-portfolio",
+	GITHUB_PORTFOLIO_URL: "https://github.com/nextglabs/nextglabs.com",
 
 	// Medium
 	MEDIUM_ACCOUNT_URL: "https://nextglabs.medium.com",
