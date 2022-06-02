5dc710aee57d8939dcb7409540363f44621a9de7
Bassem Allani <allani.bassem@yahoo.fr>
2022-06-02 14:20:16 +0200

    fix: Fixed wrong URL in dynamic links & updated dynamic component types

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,7 +2,7 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
-import { Box, Center, Heading, Link, Text, VStack, HStack } from "@chakra-ui/react";
+import { Box, Center, Heading, Link, Text, VStack, HStack, LinkProps } from "@chakra-ui/react";
 import { fetcher } from "@/config/swr";
 import { GET_PAGES_QUERY } from "@/graphql/queries/getPages";
 import { GET_PAGE_QUERY } from "@/graphql/queries/getPage";
@@ -10,14 +10,14 @@ import React from "react";
 import { Memoji } from "@/components/Memoji";
 
 const components = {
-  h3: (props) => (
-    <Heading as="h3" size="md" my="12" textAlign="left">
-      <span className="underline">{props.children}</span>
+  h3: ({ children, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h3" size="md" my="12" textAlign="left" {...props}>
+      <span className="underline">{children}</span>
     </Heading>
   ),
-  a: (props) => (
-    <Link href="https://devhausleipzig.de/" variant="colored" isExternal>
-      {props.children}
+  a: ({ children, href, ...props }: LinkProps) => (
+    <Link href={href} variant="colored" isExternal {...props}>
+      {children}
     </Link>
   ),
   p: Text,
@@ -35,7 +35,6 @@ interface IPage {
 }
 
 export default function Page({ page }) {
-  console.log(page.content);
   return (
     <Center>
       <Box px={0} maxW="container.md" textAlign="left">
