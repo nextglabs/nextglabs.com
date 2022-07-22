3732095be1823fbae51251fa4b6fc13a4d96f523
Bassem Allani <allani.bassem@yahoo.fr>
2022-07-22 17:27:15 +0200

    fix: Added missing status in api response

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     await res.revalidate(path.join("/", req.body.data.slug));
-    return res.json({ revalidated: true });
+    return res.status(200).json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
   }
