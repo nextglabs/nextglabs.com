d4d5764035e334715ecda70af5b56dc6d9d1f575
Bassem Allani <allani.bassem@yahoo.fr>
2022-07-22 19:06:54 +0200

    feat: Persist elements after animation

--- src/components/ui/animation/Transitions.tsx
+++ src/components/ui/animation/Transitions.tsx
@@ -1,41 +1,34 @@
 import { useEffect } from "react";
-import { SlideFade } from "@chakra-ui/react";
 import { useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { MotionBox } from "./Motion";
 
-export const PageSlideFade = ({ children }) => {
-	return <SlideFade in>{children}</SlideFade>;
-};
-
 const dynamicOrderVariants = {
-	visible: (custom = 0) => ({
-		opacity: 1,
-		transition: { delay: custom * 0.2 },
-		translateY: 0,
-	}),
-	hidden: { opacity: 0, translateY: 50 },
+  visible: (custom = 0) => ({
+    opacity: 1,
+    transition: { delay: custom * 0.2 },
+    translateY: 0,
+  }),
+  hidden: { opacity: 0, translateY: 50 },
 };
 
 export const SlideUpTransition = ({ children, order = 0 }) => {
-	const [ref, inView] = useInView();
-	const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const controls = useAnimation();
 
-	useEffect(() => {
-		if (inView) {
-			controls.start("visible");
-		} else {
-			controls.start("hidden");
-		}
-	}, [controls, inView]);
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
 
-	return (
-		<MotionBox ref={ref} custom={order} variants={dynamicOrderVariants} initial="hidden" animate={controls}>
-			{children}
-		</MotionBox>
-	);
+  return (
+    <MotionBox ref={ref} custom={order} variants={dynamicOrderVariants} initial="hidden" animate={controls}>
+      {children}
+    </MotionBox>
+  );
 };
 
 export const HoverTransition = ({ children }) => {
-	return <MotionBox whileHover={{ translateY: -5 }}>{children}</MotionBox>;
+  return <MotionBox whileHover={{ translateY: -5 }}>{children}</MotionBox>;
 };
