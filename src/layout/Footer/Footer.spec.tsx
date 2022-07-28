4730d9a102abd630b9b5372c82b0c00c82555b69
Bassem Allani <allani.bassem@yahoo.fr>
2022-07-28 15:31:23 +0200

    feat: Updated footer

--- src/layout/Footer/Footer.spec.tsx
+++ src/layout/Footer/Footer.spec.tsx
@@ -3,17 +3,17 @@ import { render, screen } from "@testing-library/react";
 import { Footer } from ".";
 
 describe("<Footer />", () => {
-	it("Displays copyright & message", () => {
-		render(<Footer />);
-		screen.getByText(/Copyright Nextglabs/i);
-		screen.getByText(new RegExp(new Date().getFullYear().toString(), "g"));
-		screen.getByText(/Created with ❤️ using React & NextJS/i);
-	});
+  it("Displays copyright & message", () => {
+    render(<Footer />);
+    screen.getByText(/Copyright NextGLabs/i);
+    screen.getByText(new RegExp(new Date().getFullYear().toString(), "u"));
+    screen.getByText(/Created with ❤️ using React & NextJS/i);
+  });
 
-	it("Links to repository", () => {
-		render(<Footer />);
-		const link = screen.getByText(/View Source/i);
-		expect(link).toHaveAttribute("href", SOCIAL.GITHUB_PORTFOLIO_URL);
-		expect(link).toHaveAttribute("target", "_blank");
-	});
+  it("Links to repository", () => {
+    render(<Footer />);
+    const link = screen.getByText(/View Source/i);
+    expect(link).toHaveAttribute("href", SOCIAL.GITHUB_PORTFOLIO_URL);
+    expect(link).toHaveAttribute("target", "_blank");
+  });
 });
--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -5,15 +5,15 @@ import { layoutDimensions } from "../dimensions";
 const currentYear = new Date().getFullYear();
 
 export const Footer = (props: FlexProps) => (
-	<Box mt="8" py="8" px={layoutDimensions.px} bgColor="transparent" as="footer" {...props}>
-		<Flex w={layoutDimensions.width} mx="auto" justifyContent="space-between">
-			<Text fontSize="sm">&copy; Copyright Nextglabs {currentYear}.</Text>
-			<Text fontSize="sm" textAlign="right">
-				Created with ❤️ using React &amp; NextJS.{" "}
-				<Link variant="colored" href={SOCIAL.GITHUB_PORTFOLIO_URL} isExternal>
-					View Source.
-				</Link>
-			</Text>
-		</Flex>
-	</Box>
+  <Box mt="8" py="8" px={layoutDimensions.px} bgColor="transparent" as="footer" {...props}>
+    <Flex w={layoutDimensions.width} mx="auto" justifyContent="space-between">
+      <Text fontSize="sm">&copy; Copyright NextGLabs {currentYear}.</Text>
+      <Text fontSize="sm" textAlign="right">
+        Created with ❤️ using React &amp; NextJS.{" "}
+        <Link variant="colored" href={SOCIAL.GITHUB_PORTFOLIO_URL} isExternal>
+          View Source.
+        </Link>
+      </Text>
+    </Flex>
+  </Box>
 );
