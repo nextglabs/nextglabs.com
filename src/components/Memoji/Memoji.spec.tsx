af814dd15085033737fa304cc380389edd210c6e
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-16 22:50:23 +0100

    test: Updated Memoji unit tests

--- src/components/Memoji/Memoji.spec.tsx
+++ src/components/Memoji/Memoji.spec.tsx
@@ -1,8 +1,10 @@
-import { render } from "@/utils/testUtils";
+import { render, screen } from "@/utils/testUtils";
 import { Memoji } from ".";
 
 describe("<Memoji />", () => {
-	it("renders without crashing", () => {
-		render(<Memoji />);
-	});
+  it("renders without crashing", () => {
+    render(<Memoji alt="memoji" src="/assets/images/memoji-surprised.png" />);
+    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveAttribute("src", "/_next/image?url=%2Fassets%2Fimages%2Fmemoji-surprised.png&w=256&q=75");
+  });
 });
