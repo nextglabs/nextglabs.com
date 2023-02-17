a2e62b330fc659e783a8ec4fdea910a447b90032
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-17 20:07:00 +0100

    feat: Created Logo component + added to Header

--- src/layout/Header/Nav/Mobile.tsx
+++ src/layout/Header/Nav/Mobile.tsx
@@ -1,77 +1,57 @@
-import {
-	IconButton,
-	useColorModeValue,
-	Menu,
-	MenuButton,
-	MenuList,
-	MenuItem,
-	MenuItemProps,
-	VStack,
-} from "@chakra-ui/react";
-import { FiMenu, FiX } from "react-icons/fi";
+import { Logo } from "@/components/Logo";
+import { MenuItem } from "@/components/Menu/Item";
+import { MenuList } from "@/components/Menu/List";
+import { NAV } from "@/config/nav";
+import { useSound } from "@/hooks/useSound";
+import { HStack, IconButton, Menu, MenuButton, MenuItemProps } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { NAV } from "@/config/nav";
+import { useTranslation } from "react-i18next";
+import { FiMenu, FiX } from "react-icons/fi";
 
 interface NavMenuButtonProps extends MenuItemProps {
-	index?: number;
-	label: string | JSX.Element;
-	href: string;
+  index?: number;
+  label: string;
+  href: string;
 }
 
 const NavMenuButton = ({ label, href, ...restProps }: NavMenuButtonProps) => {
-	const router = useRouter();
-	const isCurrent = router.asPath === href;
-	return (
-		<NextLink href={href}>
-			<MenuItem
-				borderRadius="6"
-				isDisabled={isCurrent}
-				isFocusable={!isCurrent}
-				fontWeight={isCurrent && "medium"}
-				_hover={{
-					cursor: isCurrent ? "initial" : "pointer",
-				}}
-				aria-current={isCurrent ? "page" : undefined}
-				{...restProps}
-			>
-				{label}
-			</MenuItem>
-		</NextLink>
-	);
+  const router = useRouter();
+  const { t } = useTranslation("common");
+  const isCurrent = router.asPath === href;
+  return (
+    <MenuItem as={NextLink} href={href} isDisabled={isCurrent} aria-current={isCurrent ? "page" : undefined} {...restProps}>
+      {t(label)}
+    </MenuItem>
+  );
 };
 
 export const HeaderNavMobile = () => {
-	const menuBorderColor = useColorModeValue("cyan.100", "cyan.200");
-	const menuBgColor = useColorModeValue("", "gray.900");
-	return (
-		<Menu isLazy autoSelect={false}>
-			{({ isOpen }) => (
-				<>
-					<MenuButton
-						as={IconButton}
-						size="md"
-						icon={isOpen ? <FiX /> : <FiMenu />}
-						aria-label="Open Menu"
-						display={["inherit", "inherit", "none"]}
-						variant="ghost"
-					/>
-					<MenuList
-						boxShadow="md"
-						borderWidth="2px"
-						borderColor={menuBorderColor}
-						bgColor={menuBgColor}
-						p="3"
-						borderRadius="12"
-					>
-						<VStack spacing="1">
-							{NAV.map((item, index) => (
-								<NavMenuButton key={index} {...item} />
-							))}
-						</VStack>
-					</MenuList>
-				</>
-			)}
-		</Menu>
-	);
+  const [play] = useSound();
+
+  return (
+    <HStack spacing={4} alignItems="center">
+      <Menu isLazy autoSelect={false} onClose={play} onOpen={play}>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              as={IconButton}
+              size="md"
+              ml={-3}
+              icon={isOpen ? <FiX /> : <FiMenu />}
+              aria-label="Open Menu"
+              display={["inherit", "inherit", "none"]}
+              variant="ghost"
+            />
+            <MenuList>
+              {NAV.map((item, index) => (
+                <NavMenuButton key={index} {...item} />
+              ))}
+            </MenuList>
+          </>
+        )}
+      </Menu>
+      <Logo />
+    </HStack>
+  );
 };
