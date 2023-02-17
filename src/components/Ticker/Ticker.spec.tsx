0d1458da0a70f25ea98c70d4985837e995bc3ef5
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-17 15:06:03 +0100

    test: Added Ticker unit tests

--- src/components/Ticker/Ticker.spec.tsx
+++ src/components/Ticker/Ticker.spec.tsx
@@ -1,8 +1,13 @@
-import { render } from "@/utils/testUtils";
+import { TICKER_KEYS } from "@/config/services";
+import { screen, render } from "@/utils/testUtils";
 import { Ticker } from "./";
 
 describe("<Ticker />", () => {
-  it("Renders without crashing", () => {
+  it("Renders ticker keywords & separators", () => {
     render(<Ticker />);
+    TICKER_KEYS.forEach((key) => {
+      expect(screen.getAllByText(`services.items.${key}.title`)).toHaveLength(4);
+    });
+    expect(screen.getAllByText("•")).toHaveLength(TICKER_KEYS.length * 4);
   });
 });
