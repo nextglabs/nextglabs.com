d82e9500f4310d75a460812b7af9710b9b81e5d2
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-19 13:22:32 +0100

    config: Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 // @ts-check
 const { i18n } = require("./next-i18next.config.js");
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE_BUNDLE === "true",
+});
+
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = {
+module.exports = withBundleAnalyzer({
   i18n,
   images: {
     domains: ["media.graphcms.com", "media.graphassets.com"],
   },
   swcMinify: true,
-};
+});
