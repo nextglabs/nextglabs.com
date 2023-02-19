eadec986e6d325a57394dd1a1b2c36023b28dac6
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-19 20:27:21 +0100

    ci(fix): Fix font modules not found

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
-import localFont from "@next/font/local";
 
 // Polyfill
 import "intersection-observer";
@@ -10,34 +9,6 @@ import theme from "@/theme";
 
 import { appWithTranslation } from "next-i18next";
 import { ThemeProvider } from "@/components/ThemeProvider";
-
-export const circluarStd = localFont({
-  src: [
-    {
-      path: "./fonts/CircularStd-book.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/CircularStd-medium.woff2",
-      weight: "500",
-      style: "medium",
-    },
-    {
-      path: "./fonts/CircularStd-bold.woff2",
-      weight: "700",
-      style: "bold",
-    },
-    {
-      path: "./fonts/CircularStd-bold.woff2",
-      weight: "900",
-      style: "black",
-    },
-  ],
-  display: "swap",
-  fallback: ["system-ui, sans-serif"],
-  adjustFontFallback: "Arial",
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/theme/fonts.tsx
+++ src/theme/fonts.tsx
@@ -1,4 +1,32 @@
-import { circluarStd } from "@/pages/_app";
+import localFont from "@next/font/local";
+
+const circluarStd = localFont({
+  src: [
+    {
+      path: "../../public/assets/fonts/CircularStd-Book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/CircularStd-Medium.woff2",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "../../public/assets/fonts/CircularStd-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "../../public/assets/fonts/CircularStd-Black.woff2",
+      weight: "900",
+      style: "black",
+    },
+  ],
+  display: "swap",
+  fallback: ["system-ui, sans-serif"],
+  adjustFontFallback: "Arial",
+});
 
 export const fonts = {
   heading: circluarStd.style.fontFamily,
