5936514d07534bc5038039e808170039208795a5
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-19 20:15:44 +0100

    ci(fix): Fixed failed CI build

--- src/theme/fonts.tsx
+++ src/theme/fonts.tsx
@@ -1,32 +1,4 @@
-import localFont from "@next/font/local";
-
-const circluarStd = localFont({
-  src: [
-    {
-      path: "./fonts/CircularStd-book.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/CircularStd-medium.woff2",
-      weight: "500",
-      style: "medium",
-    },
-    {
-      path: "./fonts/CircularStd-bold.woff2",
-      weight: "700",
-      style: "bold",
-    },
-    {
-      path: "./fonts/CircularStd-bold.woff2",
-      weight: "900",
-      style: "black",
-    },
-  ],
-  display: "swap",
-  fallback: ["system-ui, sans-serif"],
-  adjustFontFallback: "Arial",
-});
+import { circluarStd } from "@/pages/_app";
 
 export const fonts = {
   heading: circluarStd.style.fontFamily,
