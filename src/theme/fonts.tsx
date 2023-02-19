e120b52d456a4b77f3e3ea8075375cf2779b24b2
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-19 19:56:07 +0100

    ci(fix): Fixed failed CI build

--- src/theme/fonts.tsx
+++ src/theme/fonts.tsx
@@ -3,22 +3,22 @@ import localFont from "@next/font/local";
 const circluarStd = localFont({
   src: [
     {
-      path: "../../public/assets/fonts/CircularStd-book.woff2",
+      path: "./fonts/CircularStd-book.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/CircularStd-medium.woff2",
+      path: "./fonts/CircularStd-medium.woff2",
       weight: "500",
       style: "medium",
     },
     {
-      path: "../../public/assets/fonts/CircularStd-bold.woff2",
+      path: "./fonts/CircularStd-bold.woff2",
       weight: "700",
       style: "bold",
     },
     {
-      path: "../../public/assets/fonts/CircularStd-bold.woff2",
+      path: "./fonts/CircularStd-bold.woff2",
       weight: "900",
       style: "black",
     },
