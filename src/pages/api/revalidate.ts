a82f578f6e3d1a57bf7aebc341c53abb4538ab52
Bassem Allani <allani.bassem@yahoo.fr>
2023-02-19 20:20:27 +0100

    feat: Revalidate DE routes

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     await res.revalidate(path.join("/", req.body.data.slug));
+    await res.revalidate(path.join("/de", req.body.data.slug));
     return res.status(200).json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
