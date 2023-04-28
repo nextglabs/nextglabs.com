de8ee3d00776cb865ea92fa06d7c5e800a5ebb29
Bassem Allani <allani.bassem@yahoo.fr>
2023-04-28 21:28:22 +0200

    feat: Added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,18 @@ import { AppProps } from "next/app";
 // Polyfill
 import "intersection-observer";
 
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { configSEO } from "@/config/seo";
 import theme from "@/theme";
-
+import { Analytics } from "@vercel/analytics/react";
 import { appWithTranslation } from "next-i18next";
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <DefaultSeo {...configSEO} />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
