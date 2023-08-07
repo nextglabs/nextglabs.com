0018922ba169045cb643996d400a153ac7727453
Bassem Allani <allani.bassem@yahoo.fr>
2023-08-07 13:09:50 +0200

    fix: Fix revalidate endpoint failing when slug is missing

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,21 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { body, query } = req;
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+  if (query?.secret !== process.env.REVALIDATE_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
-  if (!req.body) {
+  if (!body) {
     return res.status(422).json({ message: "Invalid request body" });
   }
-
   try {
-    await res.revalidate(path.join("/", req.body.data.slug));
-    await res.revalidate(path.join("/de", req.body.data.slug));
+    const pathToRevalidate = body.__typename === "Page" ? body.data.slug : "";
+    await res.revalidate(path.join("/", pathToRevalidate));
+    await res.revalidate(path.join("/de", pathToRevalidate));
     return res.status(200).json({ revalidated: true });
   } catch (err) {
-    return res.status(500).send("Error revalidating");
+    return res.status(500).json({ message: "Error revalidating" });
   }
 }
